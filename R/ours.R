a25d80c790f8a6580160f3f8ff6341e4edd36d2a
Tristan <thrawst88@gmail.com>
2017-09-04 16:45:15 +0200

    added files

---
+++ R/ours.R
@@ -0,0 +1,19 @@
+aStarSearch <- function(start, goal) {
+  visited = list()
+  frontier = list()
+
+# returns a list of moves to get to goal
+}
+
+# for each package:
+#   run astar for the pickup and delivery
+#
+# end
+ourDeliveryMan <- function(roads, car, packages) {
+  for (i in length(packages)) {
+
+  }
+  nextMove = 1
+  car$nextMove = nextMove
+  return (car)
+}
