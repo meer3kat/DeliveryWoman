8a10d0ae8f60c5eb1b25f526bcd1f9eead2758e0
Tristan <thrawst88@gmail.com>
2017-09-05 12:00:43 +0200

    added manhattanCost heuristic

--- R/ours.R
+++ R/ours.R
@@ -14,10 +14,36 @@ if (car$directions != NULL &&
 
 debug = F
 
-# returns a list of optimal moves to get to goal
-aStarSearch <- function(roads, start, goal) {
+findCost <- function(roadSet, start, end) {
+  val = 0
+  while(start != end) {
+    if (start < end) {
+      val += roads$hroads[start]
+      start += 1
+    } else if (start > goal) {
+      val += roads$hroads[start]
+      start -= 1
+    }
+  }
+  return val
+}
+
+manhattanCost <- function(roads, start, goal) {
+  retX = findCost(roads$hroads, start$x, goal$x)
+  retY = findCost(roads$vroads, start$y, goal$y)
+
+  return retX + retY
+}
+
+# returns a list of moves to get to goal
+aStarSearch <- function(roads, carmem, start, goal) {
   visited = c()
   frontier = c()
+
+  cameFrom = carmem
+
+  gScore = list(start=0)
+  fScore = list(start=H(0))
 
   return 5
 }
@@ -27,7 +53,7 @@ ourDeliveryMan <- function(roads, car, packages) {
     print(packages)
   }
 
-  for (i in length(packages) {
+  for (i in length(packages)) {
     package = packages[i,] # ith package
     path = aStarSearch(roads, list(x=package[0], y=package[1]), list(x=package[2], y=package[3]))
     car$nextMove = nextMove
