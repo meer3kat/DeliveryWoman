f05170b3c362b1eb3e49763f44e6a1041399b0a6
Tristan <thrawst88@gmail.com>
2017-09-08 11:57:28 +0200

    runs half the time issues: - not checking visted state enough - path not optimal

--- R/ours.R
+++ R/ours.R
@@ -14,49 +14,41 @@ if (car$directions != NULL &&
 
 debug = T
 
-findCost <- function(roadSet, start, end) {
-  val = 0
-  # print(paste(roadSet, start, end, sep="_"))
-  while(start != end) {
-    if (start < end) {
-      val = val + roadSet[start]
-      start = start + 1
-    } else if (start > end) {
-      val = val + roadSet[start]
-      start = start - 1
-    }
-  }
+findTurnCost <- function(roadSet, start, end) {
+  val = sum(roadSet[start:end])
+  # print(paste(start, end, "=", val, sep="_"))
   return (val)
 }
 
 manhattanCost <- function(roads, start, goal) {
-  retX = findCost(roads$hroads, start$x, goal$x)
-  retY = findCost(roads$vroads, start$y, goal$y)
+  if (debug) print(paste("manhattanCost", start, goal))
+  retX = findTurnCost(roads$hroads, start[1], goal[1])
+  retY = findTurnCost(roads$vroads, start[2], goal[2])
   return (retX + retY)
 }
 
 # given roads and position finds the lowest path to the next node
 leastCostToNextNode <- function(roads, pos, prev) {
-  nodes = list(left=roads$hroads[pos$x] - 1,
-    right=roads$hroads[pos$x] + 1,
-    top=roads$vroads[pos$y] + 1,
-    bottom=roads$vroads[pos$y]-1
+  nodes = list(left=roads$hroads[pos[1]] - 1,
+    right=roads$hroads[pos[1]] + 1,
+    up=roads$vroads[pos[2]] + 1,
+    down=roads$vroads[pos[2]]-1
   )
   minPath = Inf
   pathKey = ""
   for (i in names(nodes)) {
     # some of these values may be "off road" and thus null
-    if (is.integer(nodes[i]) && nodes[i] < minPath) {
+    if (is.element(i, names(nodes)) && nodes[i] < minPath) {
       minPath = nodes[i]
       pathKey = i
     }
   }
 
   switch(pathKey,
-    left={ return (list(nodes[i], pos$y)) },
-    right={ return (list(nodes[i], pos$y)) },
-    top={ return (list(pos$x, nodes[i])) },
-    bottom={ return (list(pos$x, nodes[i])) },
+    left={ return (c(nodes[i], pos[2])) },
+    right={ return (c(nodes[i], pos[2])) },
+    up={ return (c(pos[1], nodes[i])) },
+    down={ return (c(pos[1], nodes[i])) },
     { print("something went wrong finding leastCostToNextNode") }
   )
 }
@@ -65,55 +57,65 @@ leastCostInFrontier <- function(frontier, fScore) {
   minPath = Inf
   pathKey = ""
   for (i in names(frontier)) {
-    if (!is.null(fScore[i]) && fScore[i] < minPath) {
-      minPath = frontier[i]
+    # if (debug) print(paste("front", frontier[i], "fscore", fScore[i], sep=","))
+    if (debug) print(paste("fs", class(fScore[[i]]), "mp", minPath, sep=","))
+    if (is.element(i, names(fScore)) && fScore[[i]] < minPath) {
+      minPath = fScore[i]
       pathKey = i
     }
   }
-  print(paste("front, fscore", frontier, fScore, pathKey, sep="-"))
-  return (fScore[pathKey])
+  return (frontier[[pathKey]])
 }
 
-getNeighborNodes <- function(pos, prev) {
-  neighbors = list(left=list(x=pos$x - 1, y=pos$y),
-             right=list(x=pos$x + 1, y=pos$y),
-               top=list(x=pos$x, y=pos$y + 1),
-            bottom=list(x=pos$x, y=pos$y - 1)
+getNeighborNodes <- function(pos, prev=NULL) {
+  neighbors = list(left=c(pos[1] - 1, pos[2]),
+             right=c(pos[1] + 1, pos[2]),
+               up=c(pos[1], pos[2] + 1),
+            down=c(pos[1], pos[2] - 1)
   )
-  return (Filter(function(x) nodesEqual(x, prev) == F, neighbors))
+  if (!is.null(prev)) {
+    prev = paste(prev)
+    switch(prev,
+      "4"={neighbors$left  = NULL},
+      "6"={neighbors$right = NULL},
+      "8"={neighbors$up    = NULL},
+      "2"={neighbors$down  = NULL}
+    )
+  }
+  return (neighbors)
 }
 
 nodesEqual <- function(n1, n2) {
-  print(paste("nodesX", n1$x, n2$x, sep="-"))
-  print(paste("nodesY", n1$y, n2$y, sep="-"))
-  return (n1$x == n2$x && n1$y == n2$y)
+  # if (debug) print(paste("nodesX", n1[1], n2[1], sep="-"))
+  # if (debug) print(paste("nodesY", n1[2], n2[2], sep="-"))
+  return (n1[1] == n2[1] && n1[2] == n2[2])
 }
 
 # list(x=1, y=3) => '1 3'
 nodeKey <- function(node) {
-  return (paste(node$x, node$y, sep="_"))
+  return (paste(node[1], node[2], sep="_"))
 }
 
 # returns a list of moves to get to goal
 aStarSearch <- function(roads, start, goal, h=manhattanCost) {
   startKey = nodeKey(start)
-  visited = list()
-  frontier = list()
+  visited = c() # list of visited nodes as keys
+  frontier = list() # key:vector
   frontier[[startKey]] = start
 
   path = c()
 
   # f(n) = g(n) + h(n)
-  gScore = list()
+  gScore = list() # key:int
   gScore[startKey] = 0
 
-  fScore = list()
+  fScore = list() # key:int
   fScore[startKey] = h(roads, start, goal)
 
   # BEGIN LOOP
   while(length(gScore) > 0) {
     current = leastCostInFrontier(frontier, fScore)
-    print(paste("current", current, sep=" "))
+    if (debug) print(paste("current", current, sep=" "))
     if (nodesEqual(current, goal)) {
       # append final move to path
       return (path)
@@ -122,41 +124,47 @@ aStarSearch <- function(roads, start, goal, h=manhattanCost) {
     # delete from frontier map
     frontier[nodeKey(current)] = NULL
 
-    # add to visited map
-    visited[nodeKey(current)] = T
+    # add to visited list
+    visited = append(visited, nodeKey(current))
 
-    neighbors = getNeighborNodes(pos, tail(path, 1))
+    neighbors = getNeighborNodes(current, tail(path, 1))
     for (i in names(neighbors)) {
-      neighbor = neighbors[i]
+      neighbor = neighbors[[i]]
       curKey = nodeKey(neighbor)
       # Ignore the neighbor which is already evaluated.
-      if (visited[curKey]) {
+      if (is.element(curKey, visited)) {
         next
       }
 
       # Discover a new node
-      if (!is.string(frontier[curKey])) {
-        frontier[curKey] = neighbor
+      neighborKey = nodeKey(neighbor)
+      if (!is.element(neighborKey, names(frontier))) {
+        frontier[[neighborKey]] = neighbor
       }
 
       # The distance from start to a neighbor
-      tmp_gScore = gScore[current] + dist_between(current, neighbor)
+      dist = findTurnCost(roads$hroads, current[1], neighbor[1]) +
+        findTurnCost(roads$vroads, current[2], neighbor[2])
+      tmp_gScore = gScore[[nodeKey(current)]] + dist
 
       # This is not a better path.
-      if (tmp_gScore >= gScore[neighbor]) {
+      if (is.integer(gScore[[neighborKey]]) &&
+        tmp_gScore >= gScore[[neighborKey]]) {
         next
       }
 
       # This path is the best until now. Record it!
       switch(i,
-         left={path[length(path) + 1] = 4},
-        right={path[length(path) + 1] = 6},
-           up={path[length(path) + 1] = 8},
-         down={path[length(path) + 1] = 2},
-        { path[length(path) + 1] = 5 }
+         left={path = append(path, 4)},
+        right={path = append(path, 6)},
+           up={path = append(path, 8)},
+         down={path = append(path, 2)},
+        { path = append(path, 5)}
       )
-      gScore[nodeKey] = tmp_gScore
-      fScore[nodeKey] = gScore[nodeKey] + h(roads, neighbor, goal)
+      print(tmp_gScore)
+      gScore[neighborKey] = tmp_gScore
+      print(paste(tmp_gScore, h(roads, neighbor, goal), sep="__"))
+      fScore[neighborKey] = tmp_gScore + h(roads, neighbor, goal)
     }
   }
 
@@ -172,12 +180,12 @@ ourDeliveryMan <- function(roads, car, packages) {
   # for (i in length(packages)) {
   package = packages[1,] # ith package
   path = aStarSearch(roads,
-    list(x=package[1], y=package[2]),
-    list(x=package[3], y=package[4]))
+    c(package[1], package[2]),
+    c(package[3], package[4]))
   #   car$nextMove = nextMove
   #   return (car)
   # }
   print(path)
 }
 
-runDeliveryMan(carReady=ourDeliveryMan)
+runDeliveryMan(carReady=ourDeliveryMan, doPlot=F)
