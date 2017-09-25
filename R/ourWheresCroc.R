59a806fed665b2eebe36d449b6a37b87da29d90d
Tristan <thrawst88@gmail.com>
2017-09-25 16:53:07 +0200

    psuedo code, does nothing

---
+++ R/ourWheresCroc.R
@@ -0,0 +1,7 @@
+'''
+salinity, nitrogen, phosphates
+for each turn
+  import S, N, P into observables layer for some HNN
+  solve forward, find most likely watering hole the croc is in
+  go to hole, search.
+'''
