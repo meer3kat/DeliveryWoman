0780bce17061cae3bc0f44b6c4e4fbe791cb611a
Peili Guo <pegu7645@user.uu.se>
2017-09-26 22:39:30 +0200

    0

--- R/ourWheresCroc.R
+++ R/ourWheresCroc.R
@@ -5,3 +5,37 @@ for each turn
   solve forward, find most likely watering hole the croc is in
   go to hole, search.
 '''
+
+
+#transition matrix
+#function get transition matrix and then write individual values in it. 
+getTransitionMatrix=function(point,edges){
+  edges=getEdges()
+  points=getPoints()
+  A=matrix(0,nrow=40,ncol=40)
+  
+  for( k in 1:nrow(A)){
+    options=getOptions(k,edges)
+    transitionP=1/length(options)
+    
+    for (j in 1:length(options)){
+      A[k,options[j]] = transitionP
+    }
+  }
+  
+  return (A)
+}
+
+#creating vector for store probability of S, N, P
+prosal= c(rep(0,40))
+pronit=c(rep(0,40))
+propoh=c(rep(0,40))
+protot=c(rep(0,40))
+
+# here we get the probability of the reading from our croc that belongs to the waterhole j's distribution.
+for (j in 1:40){
+  prosal[j]=dnorm(readings[1],probs$salinity[j,1],probs$salinity[j,2])
+  pronit[j]=dnorm(readings[2],probs$nitrogen[j,1],probs$nitrogen[j,2])
+  propoh[j]=dnorm(readings[3],probs$phosphate[j,1],probs$phosphate[j,2])
+  protot[j]=prosal[j]*pronit[j]*propoh[j]
+}
